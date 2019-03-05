01ee35889af8ec4152060d91c51eabf148f3ff42
masha8353 <atofot64123835>
2019-03-05 21:37:19 +0300

    matrix

---
+++ src/test/java/Matrix.java
@@ -0,0 +1,18 @@
+public class Matrix {
+    public static void main (String[] args){
+        int[][] matrix;
+        matrix = new int[2][3];
+
+        matrix[0][0] =1;
+        matrix[0][1] = -2;
+        matrix[0][2] = 3;
+        matrix [1][0] = 4;
+        matrix [1][1] = 1;
+
+    }
+}
+
+
+
+
+
